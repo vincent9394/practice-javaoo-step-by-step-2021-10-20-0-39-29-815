11b0076e581296a5397b297d73984550b571d434
vincent <luk96515@gmail.com>
2021-10-20 16:53:21 +0800

    [OOPpractice][vincent] spec: complete ex5

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,26 @@
 package practice05;
 
-public class Teacher {
+import practice06.Klass;
+
+public class Teacher extends Person {
+    private int klass ;
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        this(name, age,0);
+    }
+    public int getKlass() {
+
+        return klass;
+    }
+
+    public String introduce(){
+        if (! (klass==0)) {
+            return String.format("My name is %s. I am %d years old. I am a Teacher. I teach Class %d.", getName(), getAge(), getKlass());
+           }
+        return String.format("My name is %s. I am %d years old. I am a Teacher. I teach No Class.", getName(), getAge());
+        }
 }
