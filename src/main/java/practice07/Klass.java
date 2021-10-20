85a10898e97793719ad8c3f0d49dd3f5bdaabaa5
vincent <luk96515@gmail.com>
2021-10-20 21:55:18 +0800

    [OOPpractice][vincent] spec: complete ex7 excluded test should_student_introduce_with_class() and should_person_with_same_id_be_same_one()

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,26 @@
 package practice07;
 
 public class Klass {
+    private Integer number;
+    private Student leader;
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class "+number;
+    }
+
+    public Student assignLeader(Student leader){
+       return this.leader=leader;
+    }
+
+    public Student getLeader(){
+        return leader;
+    }
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,34 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(int id,String name, int age) {
+        super(id,name, age);
+    }
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        this(id, name, age);
+        this.klass = klass;
+    }
+
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        if (klass == null) {
+            return super.introduce() + String.format(" I am a Teacher. I teach No Class.");
+        }
+        return super.introduce() + String.format(" I am a Teacher. I teach Class %d.", klass.getNumber());
+    }
+
+    public String introduceWith(Student student) {
+        String introduce = super.introduce() + String.format(" I am a Teacher. I");
+        introduce += klass.getNumber() == student.getKlass().getNumber() ?
+                String.format(" teach %s.", student.getName()) : String.format(" don't teach %s.", student.getName());
+        return introduce;
+    }
 }
