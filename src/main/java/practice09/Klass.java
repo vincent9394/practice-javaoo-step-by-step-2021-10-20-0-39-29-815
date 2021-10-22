189ff35b0f97ffb40024487fcffcd94fd3449c7e
vincent <luk96515@gmail.com>
2021-10-22 20:58:17 +0800

    [OOPpractice][vincent] spec: complete ex9

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -2,11 +2,13 @@ package practice09;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Klass {
     private Integer number;
     private Student leader;
     List<Student> students = new ArrayList<>();
+
     public Klass(Integer number) {
         this.number = number;
     }
@@ -16,19 +18,18 @@ public class Klass {
     }
 
     public String getDisplayName() {
-        return "Class "+number;
+        return "Class " + number;
     }
 
-    public void assignLeader(Student leader){
-        if (students.contains(leader)){
+    public void assignLeader(Student leader) {
+        if (students.contains(leader)) {
             this.leader = leader;
-        }
-        else{
+        } else {
             System.out.print("It is not one of us.\n");
         }
     }
 
-    public Student getLeader(){
+    public Student getLeader() {
         return leader;
     }
 
@@ -36,4 +37,20 @@ public class Klass {
         students.add(member);
     }
 
+    public boolean isIn(Student student) {
+        return this.equals(student.getKlass());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Klass klass = (Klass) o;
+        return Objects.equals(number, klass.number) && Objects.equals(leader, klass.leader) && Objects.equals(students, klass.students);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
 }
--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,40 +1,46 @@
 package practice09;
 
+import java.text.MessageFormat;
+import java.util.LinkedList;
+import java.util.stream.Collectors;
+
 public class Teacher extends Person {
-    private Klass klass;
 
-    public Teacher(int id,String name, int age)
-    {
-        super(id,name, age);
-    }
+    private LinkedList<Klass> klasses;
 
-    public Teacher(int id, String name, int age, Klass klass) {
+    public Teacher(int id, String name, int age, LinkedList<Klass> klasses) {
         super(id, name, age);
-        this.klass = klass;
+        this.klasses = klasses;
     }
 
-
-    public Klass getKlass() {
-        return klass;
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
     }
 
     @Override
-    public String introduce() {
-        if (klass == null) {
-            return String.format("%s I am a Teacher. I teach No Class.",super.introduce());
-        }
-        return super.introduce() + String.format(" I am a Teacher. I teach Class %d.", klass.getNumber());
+    public String introduce(){
+        return klasses !=null?
+                MessageFormat.format("{0} I am a Teacher. I teach Class {1}.",
+                        super.introduce(),
+                        klasses
+                                .stream()
+                                .map(klassValue-> String.valueOf(klassValue.getNumber()))
+                                .collect(Collectors.joining(", "))):
+                (super.introduce()+ " I am a Teacher. I teach No Class.");
     }
 
     public String introduceWith(Student student) {
-        String introduce = super.introduce() + String.format(" I am a Teacher. I");
-        introduce += klass.getNumber() == student.getKlass().getNumber() ?
-                String.format(" teach %s.", student.getName()) : String.format(" don't teach %s.", student.getName());
-        return introduce;
+        return MessageFormat.format("{0} I am a Teacher. I {1}{2}.", super.introduce(),
+                (isTeaching(student)?"teach ":"don't teach "),
+                student.getName());
     }
 
-//    public String introduceWith(Student student) {
-//        return MessageFormat.format("{0} I am a Teacher. I {1}{2}.", super.introduce(),((student.getKlass().getNumber()==klass.getNumber())?"teach ":"don't teach "),
-//                student.getName());
-//    }
+    public LinkedList<Klass> getClasses() {
+        return klasses;
+    }
+
+    public boolean isTeaching(Student student) {
+        return klasses.stream().anyMatch(klass->klass.isIn(student));
+    }
+
 }
